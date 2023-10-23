ef4146a092a0b4bd0867b639b0232db828f6459d
tanayag <tanay_agrawal@hotmail.com>
2023-10-23 13:16:37 +0530

    adding noise levels to clip embedding and adding code for gaussian blur

---
+++ utils/dataloader_train.py
@@ -0,0 +1,75 @@
+import os
+import cv2
+import json
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torch.nn import functional as F
+
+from utils import load_json, read_img, GaussianSmoothing
+
+
+class UNet128DatasetTrain(Dataset):
+    """ This class is to be used while training, where all the conditional inputs and ground
+     truth is pre-saved and are pre-processed."""
+
+    def __init__(self,
+                 ip_dir="../train/data/ip",
+                 jp_dir="../train/data/jp",
+                 jg_dir="../train/data/jg",
+                 ia_dir="../train/data/ia",
+                 ic_dir="../train/data/ic"
+                 ):
+        """
+        Get all the inputs from ../data directory in the main project directory
+        :param ip_dir: Image of target person with source clothing on. Later
+        to be used to generate zt and to be used as ground truth for training.
+        :param jp_dir: person pose embeddings from ip
+        :param jg_dir: garment pose embeddings from 'ig', ig is the source garment image
+        :param ia_dir: clothing agnostic rgb from ip
+        :param ic_dir: segmented garment from ig
+        """
+        self.ip_list = os.listdir(ip_dir)
+        self.ip_paths = [os.path.join(ip_dir, i) for i in self.ip_list]
+
+        self.jp_list = os.listdir(jp_dir)
+        self.jp_paths = [os.path.join(jp_dir, i) for i in self.jp_list]
+
+        self.jg_list = os.listdir(jg_dir)
+        self.jg_paths = [os.path.join(jg_dir, i) for i in self.jg_list]
+
+        self.ia_list = os.listdir(ia_dir)
+        self.ia_paths = [os.path.join(ia_dir, i) for i in self.ia_list]
+
+        self.ic_list = os.listdir(ic_dir)
+        self.ic_paths = [os.path.join(ic_dir, i) for i in self.ic_list]
+
+    def __len__(self):
+        return len(self.ip_list)
+
+    def __getitem__(self, item):
+        ip = read_img(self.ip_paths[item])
+        jp = load_json(self.jp_paths[item])
+        jg = load_json(self.jg_paths[item])
+        ia = read_img(self.ia_paths[item])
+        ic = read_img(self.ic_paths[item])
+
+        # As suggested in:
+        # https://jmlr.csail.mit.edu/papers/volume23/21-0635/21-0635.pdf Section 4.4
+        # sigma = torch.FloatTensor(1).uniform_(0.4, 0.6)
+
+        # ToDo: move this to training later
+
+        # smoothing2d = GaussianSmoothing(channels=3,
+        #                                 kernel_size=3,
+        #                                 sigma=sigma,
+        #                                 conv_dim=2)
+        #
+        # # using same kernel for both images
+        # ia = F.pad(ia, (1, 1, 1, 1), mode='reflect')
+        # ia = smoothing2d(ia)
+        #
+        # ip = F.pad(ip, (1, 1, 1, 1), mode='reflect')
+        # ip = smoothing2d(ip)
+
+        return ip, jp, jg, ia, ic
--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,108 @@
+import cv2
+import json
+import math
+import numbers
 
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+def load_json(json_file_path):
+    with open(json_file_path, "r") as f:
+        d = json.load(f)
+    return d
+
+
+def read_img(img_path):
+    img = cv2.imread(img_path)
+    return img
+
+
+class GaussianSmoothing(nn.Module):
+    """
+    Source: https://discuss.pytorch.org/t/is-there-anyway-to-do-gaussian-filtering-for-an-image-2d-3d-in-pytorch/12351/10?u=tanay_agrawal
+    Apply gaussian smoothing on a
+    1d, 2d or 3d tensor. Filtering is performed seperately for each channel
+    in the input using a depthwise convolution.
+    Arguments:
+        channels (int, sequence): Number of channels of the input tensors. Output will
+            have this number of channels as well.
+        kernel_size (int, sequence): Size of the gaussian kernel.
+        sigma (float, sequence): Standard deviation of the gaussian kernel.
+        dim (int, optional): The number of dimensions of the data.
+            Default value is 2 (spatial).
+    """
+    def __init__(self, channels, kernel_size, sigma, conv_dim):
+        super(GaussianSmoothing, self).__init__()
+        if isinstance(kernel_size, numbers.Number):
+            kernel_size = [kernel_size] * conv_dim
+        if isinstance(sigma, numbers.Number):
+            sigma = [sigma] * conv_dim
+
+        # The gaussian kernel is the product of the
+        # gaussian function of each dimension.
+        kernel = 1
+        meshgrids = torch.meshgrid(
+            [
+                torch.arange(size, dtype=torch.float32)
+                for size in kernel_size
+            ]
+        )
+        for size, std, mgrid in zip(kernel_size, sigma, meshgrids):
+            mean = (size - 1) / 2
+            kernel *= 1 / (std * math.sqrt(2 * math.pi)) * \
+                      torch.exp(-((mgrid - mean) / std) ** 2 / 2)
+
+        # Make sure sum of values in gaussian kernel equals 1.
+        kernel = kernel / torch.sum(kernel)
+
+        # Reshape to depthwise convolutional weight
+        kernel = kernel.view(1, 1, *kernel.size())
+        kernel = kernel.repeat(channels, *[1] * (kernel.dim() - 1))
+
+        self.register_buffer('weight', kernel)
+        self.groups = channels
+
+        if conv_dim == 1:
+            self.conv = F.conv1d
+        elif conv_dim == 2:
+            self.conv = F.conv2d
+        elif conv_dim == 3:
+            self.conv = F.conv3d
+        else:
+            raise RuntimeError(
+                'Only 1, 2 and 3 dimensions are supported. Received {}.'.format(conv_dim)
+            )
+
+    def forward(self, input):
+        """
+        Apply gaussian filter to input.
+        Arguments:
+            input (torch.Tensor): Input to apply gaussian filter on.
+        Returns:
+            filtered (torch.Tensor): Filtered output.
+        """
+        return self.conv(input, weight=self.weight, groups=self.groups)
+
+
+if __name__ == "__main__":
+    import torch
+    import time
+
+
+    torch.manual_seed(1)
+    smoothing = GaussianSmoothing(1, 3, 0.6, 2)
+    input = torch.rand(1, 1, 2, 2)
+    # print(input)
+    input_p = F.pad(input, (1, 1, 1, 1), mode='reflect')
+    since = time.time()
+    output = smoothing(input_p)
+    # print(output)
+    print(time.time() - since)
+    # smoothing = GaussianSmoothing(1, 3, (0.2, 0.6), 1)
+    # input = torch.arange(1, 4)[None, None, :].to(torch.float32)
+    # print(input)
+    # input = F.pad(input, (1, 1), mode='reflect')
+    # output = smoothing(input)
+    # print(output)
